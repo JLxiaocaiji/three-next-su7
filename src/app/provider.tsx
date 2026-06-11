93014cd94deebb56d724aa39a64083e5e0aa73cd
JLxcj <884026321@qq.com>
2026-06-11 23:19:32 +0800

    1

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -1,7 +1,5 @@
-
+import React, { useState } from 'react'
 
 export const Provider = (props: { children: React.ReactNode }) => {
-    const [queryClient] = React.useState(
-        
-    )
+  return <>{props.children}</>
 }
