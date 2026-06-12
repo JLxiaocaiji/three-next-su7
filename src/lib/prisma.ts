c28793580fa5facfc14ec6a8e7542d401dc4a2bf
JLxcj <884026321@qq.com>
2026-06-12 08:41:53 +0800

    1

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,18 +1,18 @@
-import "dotenv/config";
-import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "../generated/prisma/client";
+// import "dotenv/config";
+// import { PrismaPg } from "@prisma/adapter-pg";
+// import { PrismaClient } from "../generated/prisma/client";
 
-const connectionString = `${process.env.DATABASE_URL}`;
+// const connectionString = `${process.env.DATABASE_URL}`;
 
-console.log("connectionString", connectionString);
+// console.log("connectionString", connectionString);
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
-}
+// const globalForPrisma = globalThis as unknown as {
+//   prisma: PrismaClient | undefined
+// }
 
-const adapter = new PrismaPg({ connectionString });
-const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter })
+// const adapter = new PrismaPg({ connectionString });
+// const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter })
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+// if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
-export default prisma
+// export default prisma
